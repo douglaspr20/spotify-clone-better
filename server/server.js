e61332a698e006d4de8235354fa8318bbc581328
Jorge Ocaris <jorgeocarisaa@gmail.com>
2021-05-03 13:15:48 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+const express = require('express')
+const SpotifyWebApi = require('spotify-web-api-node')
+const cors = require('cors')
+const lyricsFinder = require('lyrics-finder')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    console.log(refreshToken)
+
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken
+    })
+
+    spotifyApi.refreshAccessToken().then(
+        (data) => {
+            res.json({
+                accessToken: data.body.access_token,
+                expireIn: data.body.expires_in
+            })
+
+            spotifyApi.setAccessToken(data.body['access_token'])
+        }).catch(e => {
+            res.sendStatus(400)
+        })
+})
+
+app.post('/login', (req, res) => {
+    const code = req.body.code
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+    })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expireIn: data.body.expires_in
+        })
+    }).catch((e) => {
+        res.sendStatus(400)
+    })
+})
+
+app.get('/lyrics', async (req, res) => {
+
+    const lyrics = await lyricsFinder(req.query.artist, req.query.track) || 'No Lyrics found'
+
+    res.json({ lyrics })
+})
+
+app.listen(3001)
+
